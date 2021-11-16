d040c8baab0d12d3ac58d478f9245237b02f6bba
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2021-11-16 13:30:29 +0000

    ported from vanilla html and css to vite/vue

---
+++ src/App.vue
@@ -0,0 +1,159 @@
+<template>
+  <main-nav></main-nav>
+  <colors-pane></colors-pane>
+</template>
+
+<script setup>
+import MainNav from "./components/MainNav.vue";
+import ColorsPane from "./components/ColorsPane.vue";
+</script>
+
+
+<style>
+/* External Fonts  */
+
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;600;700&family=Zen+Antique&display=swap");
+
+/* ----- RESET -----  */
+
+/* Box sizing rules */
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+/* Remove default margin */
+body,
+h1,
+h2,
+h3,
+h4,
+p,
+figure,
+blockquote,
+dl,
+dd {
+  margin: 0;
+}
+
+/* Remove list styles on ul, ol elements  */
+ul,
+ol {
+  list-style: none;
+}
+
+/* Set core root defaults */
+html:focus-within {
+  scroll-behavior: smooth;
+}
+
+/* Set core body defaults */
+body {
+  min-height: 100vh;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-rendering: optimizeLegibility;
+  line-height: 1.5;
+}
+
+/* A elements that don't have a class get default styles */
+a:not([class]) {
+  text-decoration-skip-ink: auto;
+}
+
+/* Make images easier to work with */
+img,
+picture {
+  max-width: 100%;
+  display: block;
+}
+
+/* Inherit fonts for inputs and buttons */
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+
+/* Remove all animations, transitions and smooth scroll for people that prefer not to see them */
+@media (prefers-reduced-motion: reduce) {
+  html:focus-within {
+    scroll-behavior: auto;
+  }
+
+  *,
+  *::before,
+  *::after {
+    animation-duration: 0.01ms !important;
+    animation-iteration-count: 1 !important;
+    transition-duration: 0.01ms !important;
+    scroll-behavior: auto !important;
+  }
+}
+
+/* Custom Properties  */
+:root {
+  --clr-main: #435;
+  --clr-complementary: #674712;
+  --clr-light: #ffc76e;
+  --clr-accent: #1662a6;
+  --clr-accent-light: #50a5f2;
+  --text-light: antiquewhite;
+  --slot-size: 100px;
+}
+
+/* ----- STYLES -----  */
+
+html {
+  background-color: var(--clr-light);
+}
+
+body {
+  font-family: "Montserrat", sans-serif;
+}
+
+/* Main  */
+
+main {
+  margin-top: 50px;
+  padding: 2rem;
+}
+
+.pallete-pane {
+  display: flex;
+  justify-content: center;
+  gap: 1.5rem;
+  border: 1px solid black;
+  border-radius: 0.5rem;
+  padding: 0.5rem;
+}
+
+.color-slot {
+  width: var(--slot-size);
+  height: var(--slot-size);
+  border: 1px solid black;
+  border-radius: 0.25rem;
+  background-color: var(--text-light);
+  display: flex;
+  gap: 5px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 0.65rem;
+  margin-bottom: 1rem;
+}
+
+.slot-buttons {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.slot-buttons button {
+  font-size: 0.75rem;
+}
+</style>
---
+++ src/components/ColorsPane.vue
@@ -0,0 +1,67 @@
+<template>
+  <main>
+    <!-- Pallete Pane  -->
+    <section class="pallete-pane">
+      <div id="slot1">
+        <div class="color-slot">
+          <span class="rgbValue"></span>
+          <span class="hexValue"></span>
+          <span class="hslValue"></span>
+        </div>
+        <div class="slot-buttons">
+          <button class="generate-color">Random</button>
+        </div>
+      </div>
+      <div id="slot2">
+        <div class="color-slot">
+          <span class="rgbValue"></span>
+          <span class="hexValue"></span>
+          <span class="hslValue"></span>
+        </div>
+        <div class="slot-buttons">
+          <button class="generate-color">Random</button>
+          <button class="complementary-color">Complement</button>
+        </div>
+      </div>
+      <div id="slot3">
+        <div class="color-slot">
+          <span class="rgbValue"></span>
+          <span class="hexValue"></span>
+          <span class="hslValue"></span>
+        </div>
+        <div class="slot-buttons">
+          <button class="generate-color">Random</button>
+        </div>
+      </div>
+      <div id="slot4">
+        <div class="color-slot">
+          <span class="rgbValue"></span>
+          <span class="hexValue"></span>
+          <span class="hslValue"></span>
+        </div>
+        <div class="slot-buttons">
+          <button class="generate-color">Random</button>
+        </div>
+      </div>
+      <div id="slot5">
+        <div class="color-slot">
+          <span class="rgbValue"></span>
+          <span class="hexValue"></span>
+          <span class="hslValue"></span>
+        </div>
+        <div class="slot-buttons">
+          <button class="generate-color">Random</button>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script setup>
+const rgb = ref("");
+const hex = ref("");
+const hsl = ref("");
+</script>
+
+<style>
+</style>
