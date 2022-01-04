7a570ba559f77e41a0eb120c2ad09196da6f56d6
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2022-01-04 22:48:58 +0000

    testing color schemes

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,12 @@
     <colors-pane></colors-pane>
     <h2 v-if="uniqueColors.size" class="heading2">Pick your variations</h2>
     <mini-slots v-if="uniqueColors.size"></mini-slots>
+    <h2 class="heading2" @click="setCssVars">Test this pallete</h2>
   </main>
 </template>
 
 <script setup>
-import { computed, ref } from "vue";
+import { computed } from "vue";
 import { useStore } from "vuex";
 
 import MainNav from "./components/MainNav.vue";
@@ -22,6 +23,26 @@ import MiniSlots from "./components/MiniSlots.vue";
 
 const store = useStore();
 const uniqueColors = computed(() => store.getters.uniqueColors);
+
+const setCssVars = () => {
+  const main = computed(() => store.state.mainSlotColor.hex);
+  const complementary = computed(() => store.state.slotColors.slot2.hex);
+  const light = computed(() => store.state.slotColors.slot3.hex);
+  const accent = computed(() => store.state.slotColors.slot4.hex);
+  const accentLight = computed(() => store.state.slotColors.slot5.hex);
+
+  document.documentElement.style.setProperty("--clr-main", main.value);
+  document.documentElement.style.setProperty(
+    "--text-light",
+    complementary.value
+  );
+  document.documentElement.style.setProperty("--clr-light", light.value);
+  document.documentElement.style.setProperty("--clr-accent", accent.value);
+  document.documentElement.style.setProperty(
+    "--clr-accent-light",
+    accentLight.value
+  );
+};
 </script>
 
 
