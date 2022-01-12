7edf5b370ff387b1beccf0b5f8e756cb77517001
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2022-01-12 18:25:58 +0000

    move utility buttons to own component

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,10 @@
     <colors-pane></colors-pane>
     <h2 v-if="uniqueColors.size" class="heading2">Pick your variations</h2>
     <mini-slots v-if="uniqueColors.size"></mini-slots>
-    <div class="utility-buttons" v-if="showPalleteButtons">
-      <button class="generate-color" @click="setCssVars">
-        Test this pallete
-      </button>
-      <button class="generate-color" @click="setLightText">Light Text</button>
-      <button class="generate-color" @click="setDarkText">Dark Text</button>
-      <button class="generate-color" @click="copyPallete">Copy Pallete</button>
-      <button class="generate-color" @click="savePallete">Save Pallete</button>
-    </div>
+    <utility-buttons
+      @copyPallete="showCopyModal = true"
+      @savePallete="showSaveModal = true"
+    />
   </main>
   <transition name="modal">
     <CopyModal v-if="showCopyModal" @close="showCopyModal = false" />
@@ -36,51 +31,12 @@ import ColorsPane from "./components/ColorsPane.vue";
 import MiniSlots from "./components/MiniSlots.vue";
 import CopyModal from "./components/CopyModal.vue";
 import SaveModal from "./components/SaveModal.vue";
-
-const showCopyModal = ref(false);
-const showSaveModal = ref(false);
+import UtilityButtons from "./components/UtilityButtons.vue";
 
 const store = useStore();
+const showCopyModal = ref(false);
+const showSaveModal = ref(false);
 const uniqueColors = computed(() => store.getters.uniqueColors);
-const showPalleteButtons = computed(() => store.getters.fullSchemeSet);
-
-const setCssVars = () => {
-  const main = computed(() => store.state.mainSlotColor.hex);
-  const complementary = computed(() => store.state.slotColors.slot2.hex);
-  const light = computed(() => store.state.slotColors.slot3.hex);
-  const accent = computed(() => store.state.slotColors.slot4.hex);
-  const accentLight = computed(() => store.state.slotColors.slot5.hex);
-
-  document.documentElement.style.setProperty("--clr-main", main.value);
-  document.documentElement.style.setProperty(
-    "--clr-complementary",
-    complementary.value
-  );
-  document.documentElement.style.setProperty("--clr-light", light.value);
-  document.documentElement.style.setProperty("--clr-accent", accent.value);
-  document.documentElement.style.setProperty(
-    "--clr-accent-light",
-    accentLight.value
-  );
-};
-
-const setLightText = () => {
-  document.documentElement.style.setProperty("--text-color", "#faebd7");
-  store.dispatch("SET_TEXT_COLOR", "light");
-};
-
-const setDarkText = () => {
-  document.documentElement.style.setProperty("--text-color", "#0f131a");
-  store.dispatch("SET_TEXT_COLOR", "dark");
-};
-
-const copyPallete = () => {
-  showCopyModal.value = true;
-};
-
-const savePallete = () => {
-  showSaveModal.value = true;
-};
 </script>
 
 
--- src/components/UtilityButtons.vue
+++ src/components/UtilityButtons.vue
@@ -1,7 +1,62 @@
-<template></template>
+<template>
+  <div class="utility-buttons" v-if="showPalleteButtons">
+    <button class="generate-color" @click="setCssVars">
+      Test this pallete
+    </button>
+    <button class="generate-color" @click="setLightText">Light Text</button>
+    <button class="generate-color" @click="setDarkText">Dark Text</button>
+    <button class="generate-color" @click="copyPallete">Copy Pallete</button>
+    <button class="generate-color" @click="savePallete">Save Pallete</button>
+  </div>
+</template>
 
-<script>
-export default {};
+<script setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+const emit = defineEmits(["copyPallete", "savePallete"]);
+
+const store = useStore();
+
+const showPalleteButtons = computed(() => store.getters.fullSchemeSet);
+
+const setLightText = () => {
+  document.documentElement.style.setProperty("--text-color", "#faebd7");
+  store.dispatch("SET_TEXT_COLOR", "light");
+};
+
+const setDarkText = () => {
+  document.documentElement.style.setProperty("--text-color", "#0f131a");
+  store.dispatch("SET_TEXT_COLOR", "dark");
+};
+
+const copyPallete = () => {
+  emit("copyPallete");
+};
+
+const savePallete = () => {
+  emit("savePallete");
+};
+
+const setCssVars = () => {
+  const main = computed(() => store.state.mainSlotColor.hex);
+  const complementary = computed(() => store.state.slotColors.slot2.hex);
+  const light = computed(() => store.state.slotColors.slot3.hex);
+  const accent = computed(() => store.state.slotColors.slot4.hex);
+  const accentLight = computed(() => store.state.slotColors.slot5.hex);
+
+  document.documentElement.style.setProperty("--clr-main", main.value);
+  document.documentElement.style.setProperty(
+    "--clr-complementary",
+    complementary.value
+  );
+  document.documentElement.style.setProperty("--clr-light", light.value);
+  document.documentElement.style.setProperty("--clr-accent", accent.value);
+  document.documentElement.style.setProperty(
+    "--clr-accent-light",
+    accentLight.value
+  );
+};
 </script>
 
 <style>
