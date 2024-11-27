b54985c98cd568f0929b0c558a030e31a46048c3
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2024-11-27 11:51:13 +0000

    fix: api key

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        define: {
+            'process.env.FIREBASE_API_KEY': JSON.stringify(
+                env.FIREBASE_API_KEY
+            ),
+        },
+        plugins: [vue()],
+    };
+});
